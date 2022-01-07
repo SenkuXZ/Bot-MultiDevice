66e82d0664c74c7d9ba817ec55d7f547e636f57b
Rapzz_ <89180287+SenkuXZ@users.noreply.github.com>
2022-01-08 00:38:36 +0700

    Typo :v

--- plugins/groupSettings.js
+++ plugins/groupSettings.js
@@ -5,7 +5,7 @@ let groupsettings = async(q , conn, M) => {
 	}[(q || '')]
   if (isClose === undefined) return conn.send2Button(M.chat, '*Get to Open/Close Group!!*', "©Senkuu", "</Open", `!group open`, "</Close", `!group close`, M)
 await conn.groupSettingUpdate(M.chat, isClose)
-M.reply('Done Open Group!!')
+M.reply('Done!!')
 }
 
 module.exports = groupsettings
