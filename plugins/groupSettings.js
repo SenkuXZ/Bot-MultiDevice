2127fda35ed7899e30723c77703987147d222c1d
Rapzz_ <89180287+SenkuXZ@users.noreply.github.com>
2022-01-18 20:06:41 +0700

    Update 1/0, on/off.

--- plugins/groupSettings.js
+++ plugins/groupSettings.js
@@ -2,6 +2,10 @@ let groupsettings = async(q , conn, M) => {
   let isClose = { 
 		'open': 'not_announcement',
 		'close': 'announcement',
+		'1': 'not_announcement',
+		'0': 'announcement',
+		'on': 'not_announcement',
+		'off': 'announcement',
 	}[(q || '')]
   if (isClose === undefined) return conn.send2Button(M.chat, '*Get to Open/Close Group!!*', "©Senkuu", "</Open", `!group open`, "</Close", `!group close`, M)
 await conn.groupSettingUpdate(M.chat, isClose)
