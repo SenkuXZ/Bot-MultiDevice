705692e8067bf99f8c1d2099eab1933eac332f6d
Rapzz_ <89180287+SenkuXZ@users.noreply.github.com>
2022-01-31 07:25:06 +0700

    Lupa nambahin mess

--- message/dasha.js
+++ message/dasha.js
@@ -57,7 +57,7 @@ moment.tz.setDefault("Asia/Jakarta").locale("id");
  
 module.exports = async(dasha, msg, mess, m, M, help, setting) => {
 try {
-const { ownerNumber, botName } = setting
+let { ownerNumber, botName } = setting
 let { type, quotedMsg, now, fromMe } = mess
 let timeout = 60000
 const time = moment(Date.now()).tz('Asia/Jakarta').locale('id').format('DD/MM/YY HH:mm:ss z')
